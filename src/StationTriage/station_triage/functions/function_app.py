825bc6ea45c70433e79d5a20d66badeb53549614
BillMeabrod <billmeabrod@gmail.com>
2026-06-18 23:17:35 -0400

    Fixing errors in function deployment.

--- src/StationTriage/station_triage/functions/function_app.py
+++ src/StationTriage/station_triage/functions/function_app.py
@@ -5,7 +5,7 @@ import sys
 
 import django
 
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, os.path.dirname(__file__))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'station_triage.settings')
 django.setup()
 
